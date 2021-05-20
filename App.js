7cb5c9285cfdadd290c03f7ceb5ea3a00ba7c418
Bekobert <bekobert@gmail.com>
2021-05-20 16:18:27 +0300

    page1 moved to an external js file

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import Page1 from './Page1/Page1';
 
 export default function App() {
-  return (
-    <View style={{flex: 1}}>
-      
-      <View style = {{backgroundColor: 'powderblue', flex: 2}} > </View>
-      <View style = {{backgroundColor: 'lightsteelblue', flex:10}} > 
-        
-        <View style = {{flex: 2, backgroundColor: 'lavender', padding: 10, margin: 30, borderRadius: 20 }} > </View>
-        <View style = {{flex: 2, backgroundColor: 'gainsboro', padding: 10, margin: 30, borderRadius: 20 }} > </View>
-        <View style = {{flex: 2, backgroundColor: 'moccasin', padding: 10, margin: 30, borderRadius: 20 }} > </View>
-
-      </View>
+  return (  
     
-
-    </View>
+    <Page1></Page1>
+    
   );
 }
 
---
+++ Page1/Page1.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const Page1 = () => {
+    return (
+    <View style={{flex: 1}}>
+      
+      <View style = {{backgroundColor: 'powderblue', flex: 2}} > </View>
+      <View style = {{backgroundColor: 'lightsteelblue', flex:10}} > 
+        
+        <View style = {{flex: 2, backgroundColor: 'lavender', padding: 10, margin: 30, borderRadius: 20 }} > </View>
+        <View style = {{flex: 2, backgroundColor: 'gainsboro', padding: 10, margin: 30, borderRadius: 20 }} > </View>
+        <View style = {{flex: 2, backgroundColor: 'moccasin', padding: 10, margin: 30, borderRadius: 20 }} > </View>
+
+      </View>
+    
+
+    </View>
+    );
+}
+
+export default Page1
